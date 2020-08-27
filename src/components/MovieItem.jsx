328f1244182263f25e9dd49fc80cecfb7ed93180
Anton <a0660576041@gmail.com>
2020-08-27 19:51:17 +0300

    feat: add card actions menu

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -7,6 +7,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import CloseIcon from '@material-ui/icons/Close';
 import PropTypes from 'prop-types';
 import baseUrl from '../utils/api';
 
@@ -49,6 +52,20 @@ const useStyles = makeStyles(theme => ({
     paddingRight: 10,
     paddingLeft: 10,
     fontSize: '0.8rem'
+  },
+  closeIcon: {
+    position: 'absolute',
+    right: 10,
+    top: 0
+  },
+  closeContainer: {
+    position: 'relative',
+    marginBottom: 25
+  },
+  menu: {
+    position: 'absolute',
+    right: 0,
+    top: 0
   }
 }));
 
@@ -57,15 +74,60 @@ export default function MovieItem({ movie }) {
   const { title, releaseDate, genres, backdropPath } = movie;
   const releaseYear = releaseDate.substr(0, 4);
   const genre = genres.join(' & ');
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleEdit = () => {
+    setAnchorEl(null);
+  };
+  const handleDelete = () => {
+    setAnchorEl(null);
+  };
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
       <Card className={classes.card}>
         <CardHeader
           className={classes.cardHeader}
           action={
-            <IconButton className={classes.iconButton}>
-              <MoreVertIcon />
-            </IconButton>
+            <>
+              <IconButton
+                aria-label="more"
+                aria-controls="long-menu"
+                aria-haspopup="true"
+                onClick={handleClick}
+                className={classes.iconButton}
+              >
+                <MoreVertIcon />
+              </IconButton>
+              <Menu
+                id="long-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={open}
+                onClose={handleClose}
+                PaperProps={{
+                  style: {
+                    width: '20ch'
+                  }
+                }}
+              >
+                <div className={classes.closeContainer}>
+                  <CloseIcon className={classes.closeIcon} onClick={handleClose} />
+                </div>
+                <MenuItem onClick={handleEdit}>Edit</MenuItem>
+                <MenuItem onClick={handleDelete}>Delete</MenuItem>
+              </Menu>
+            </>
           }
         />
         <CardMedia
