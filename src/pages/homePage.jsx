01bf83291608c2ca2a8a796e528ff4a6d7d6df66
Anton <a0660576041@gmail.com>
2020-09-06 12:38:56 +0300

    refactor HomePage into class component

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import MovieList from '../containers/MovieList';
 import Header from '../containers/Header';
@@ -7,85 +7,140 @@ import moviesList from '../utils/data';
 import { defaultMovie } from '../utils/misc';
 import helpers from '../utils/helpers';
 
-function HomePage() {
-  const [movies, setMovies] = useState(moviesList);
-  const [isEditMode, setIsEditMode] = useState(false);
-  const [openAddEditModal, setOpenAddEditModal] = useState(false);
-  const [openDeleteModal, setOpenDeleteModal] = useState(false);
-  const [selectedMovie, setSelectedMovie] = useState(null);
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+      isEditMode: false,
+      openAddEditModal: false,
+      openDeleteModal: false,
+      selectedMovie: null
+    };
+  }
 
-  const handleSort = sortBy => {
-    const sortedMovies = helpers.getSortedMovies(movies, sortBy);
-    setMovies(sortedMovies);
-  };
+  componentDidMount() {
+    this.setState({
+      movies: [...moviesList]
+    });
+  }
 
-  const handleSearch = query => {
+  setMovies(movies) {
+    this.setState({
+      movies: [...movies]
+    });
+  }
+
+  setIsEditMode(val) {
+    this.setState({
+      isEditMode: val
+    });
+  }
+
+  setOpenAddEditModal(val) {
+    this.setState({
+      openAddEditModal: val
+    });
+  }
+
+  setOpenDeleteModal(val) {
+    this.setState({
+      openDeleteModal: val
+    });
+  }
+
+  setSelectedMovie(movie) {
+    this.setState({
+      selectedMovie: { ...movie }
+    });
+  }
+
+  handleSearch = query => {
     if (query.trim() === '') {
-      setMovies(moviesList);
+      this.setMovies(moviesList);
       return;
     }
     const searchResults = moviesList.filter(movie =>
       movie.title.toUpperCase().includes(query.trim().toUpperCase())
     );
-    setMovies(searchResults);
+    this.setMovies(searchResults);
   };
 
-  const handleFilter = filter => {
+  handleFilter = filter => {
     if (filter === 'all') {
-      setMovies(moviesList);
+      this.setMovies(moviesList);
       return;
     }
     const filteredList = moviesList.filter(movie => {
       const formattedGenres = movie.genres.map(genre => genre.toUpperCase());
       return formattedGenres.includes(filter.toUpperCase());
     });
-    setMovies(filteredList);
+    this.setMovies(filteredList);
   };
 
-  const deleteMovie = () => {
-    const updatedMovieList = movies.filter(movie => movie.id !== selectedMovie.id);
-    setMovies(updatedMovieList);
-    setSelectedMovie(null);
+  handleSort = sortBy => {
+    const { movies } = this.state;
+    const sortedMovies = helpers.getSortedMovies(movies, sortBy);
+    this.setState({
+      movies: [...sortedMovies]
+    });
   };
 
-  const editMovie = editedMovie => {
+  deleteMovie() {
+    const { movies } = this.state;
+    const updatedMovieList = movies.filter(movie => movie.id !== this.selectedMovie.id);
+    this.setMovies(updatedMovieList);
+    this.setSelectedMovie(null);
+  }
+
+  editMovie(editedMovie) {
+    const { movies } = this.state;
     const foundIndex = movies.findIndex(movie => movie.id === editedMovie.id);
-    setMovies([...movies.slice(0, foundIndex, 1), editedMovie, ...movies.slice(foundIndex + 1)]);
-  };
+    const updatedList = [
+      ...movies.slice(0, foundIndex, 1),
+      editedMovie,
+      ...movies.slice(foundIndex + 1)
+    ];
+    this.setMovies(updatedList);
+  }
 
-  const addMovie = movie => {
+  addMovie(movie) {
+    const { movies } = this.state;
     const newMovie = { ...movie, id: new Date().getTime() };
-    setMovies([...movies, newMovie]);
-  };
+    this.setMovies([...movies, newMovie]);
+  }
 
-  const openModalAddMovie = () => {
-    setSelectedMovie({ ...defaultMovie });
-    setIsEditMode(false);
-    setOpenAddEditModal(true);
-  };
+  openModalAddMovie() {
+    this.setSelectedMovie(defaultMovie);
+    this.setIsEditMode(false);
+    this.setOpenAddEditModal(false);
+  }
 
-  return (
-    <>
-      <Header handleSearch={handleSearch} handleClick={openModalAddMovie} />
-      <MovieList
-        movies={movies}
-        handleSort={handleSort}
-        handleFilter={handleFilter}
-        isEditMode={isEditMode}
-        setIsEditMode={setIsEditMode}
-        openAddEditModal={openAddEditModal}
-        setOpenAddEditModal={setOpenAddEditModal}
-        openDeleteModal={openDeleteModal}
-        setOpenDeleteModal={setOpenDeleteModal}
-        deleteMovie={deleteMovie}
-        selectedMovie={selectedMovie}
-        setSelectedMovie={setSelectedMovie}
-        editMovie={editMovie}
-        addMovie={addMovie}
-      />
-      <Footer />
-    </>
-  );
+  render() {
+    const { isEditMode, openAddEditModal, openDeleteModal, selectedMovie, movies } = this.state;
+    return (
+      <>
+        <Header handleSearch={this.handleSearch} handleClick={this.openModalAddMovie} />
+        <MovieList
+          movies={movies}
+          handleSort={this.handleSort}
+          handleFilter={this.handleFilter}
+          isEditMode={isEditMode}
+          setIsEditMode={this.setIsEditMode}
+          openAddEditModal={openAddEditModal}
+          setOpenAddEditModal={this.setOpenAddEditModal}
+          openDeleteModal={openDeleteModal}
+          setOpenDeleteModal={this.setOpenDeleteModal}
+          deleteMovie={this.deleteMovie}
+          selectedMovie={selectedMovie}
+          setSelectedMovie={this.setSelectedMovie}
+          editMovie={this.editMovie}
+          addMovie={this.addMovie}
+        />
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default HomePage;
